b4a809f7a1a48f1f3cbb76656989297cdf36d72a
Denny <Dennyng1989@hotmail.com>
2023-01-25 22:37:49 -0800

    Commit self-made version of assertion test

---
+++ assertEqual.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+
+  let printOut;
+
+  if (actual === expected) {
+    printOut = '✔️ Assertion Passed: ' + actual + " === " + expected;
+  } else {
+    printOut = '❌ Assertion Failed: ' + actual + " !=== " + expected;
+  }
+  
+  console.log(printOut);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
+assertEqual("Test", "Test");
+assertEqual("Test", "test");
