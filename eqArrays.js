9077c5e641486795a0f9963f2759506b8469a203
Denny <Dennyng1989@hotmail.com>
2023-01-29 15:08:25 -0800

    commmit eqArrays.js to lotide repo

---
+++ eqArrays.js
@@ -0,0 +1,37 @@
+const assertEqual = function(actual, expected) {
+
+  let printOut;
+
+  if (actual === expected) {
+    printOut = `✔️ Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    printOut = `❌ Assertion Failed: ${actual} !== ${expected}`;
+  }
+  
+  console.log(printOut);
+};
+
+
+function eqArrays(arrayOne, arrayTwo) {
+  // compare length of array1/2
+  // this catches cases like [1, 2, 3] and [1, 2]
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+
+  // loop to compare each element of array1/2
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+
+  // only true if none of the bools are false
+  return true;
+}
+
+// test eqArrays using assertEqual
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should FAIL
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => should FAIL
