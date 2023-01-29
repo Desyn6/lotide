3e945da1716ef0733cb863d1102b289e019c3d1a
Denny <Dennyng1989@hotmail.com>
2023-01-29 15:57:05 -0800

    Commit assertArraysEqual to lotide repo

---
+++ assertArraysEqual.js
@@ -0,0 +1,32 @@
+function assertArraysEqual(arrayOne, arrayTwo) {
+  let printOut;
+
+  if (eqArrays(arrayOne, arrayTwo)) { // call eqArrays to compare arr1/2
+    printOut = `✔️ Assertion Passed: [${arrayOne}] === [${arrayTwo}]`;
+  } else {
+    printOut = `❌ Assertion Failed: [${arrayOne}] !== [${arrayTwo}]`;
+  }
+
+  console.log(printOut);
+}
+
+function eqArrays(arrayOne, arrayTwo) {
+  // compare length of array1/2
+  // this catches cases like [1, 2, 3] and [1, 2]
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+
+  // loop to compare each element of array1/2
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  
+  return true; // only true if none of the bools are false
+}
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should return PASS
+assertArraysEqual([1, 2, 3, 4], [1, 2, 3]); // => should return FAIL
+assertArraysEqual([1, 2, 3], [1, 2, '3']); // => should return FAIL
