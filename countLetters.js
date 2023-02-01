340013f0cec75749ec57d752d9197b5a3b5e0004
Denny <Dennyng1989@hotmail.com>
2023-02-01 15:56:17 -0800

    commit countLetters.js to lotide repo

---
+++ countLetters.js
@@ -0,0 +1,22 @@
+function countLetters(inputString) {
+  const noSpaces = inputString.replace(/\W/g, ''); //filter out spaces
+  const totalCount = {}; // declare empty object to populate
+
+  for (let char of noSpaces) {
+    // toLowerCase to ignore casing
+    char = char.toLowerCase();
+
+    // if char is in object, increase count
+    if (totalCount[char]) {
+      totalCount[char] += 1;
+
+      // if char is not in object, introduce to object and set to 1
+    } else if (!totalCount[char]) {
+      totalCount[char] = 1;
+    }
+  }
+  
+  return totalCount;
+}
+
+console.log(countLetters("Lighthouse in ThE HoUSe"));
