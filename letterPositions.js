af8f8d6b6b85b02fea249181d6c43d8b2509656c
Denny <Dennyng1989@hotmail.com>
2023-02-01 16:20:35 -0800

    commit letterPositions.js first rev to lotide repo

---
+++ letterPositions.js
@@ -0,0 +1,20 @@
+function letterPositions(inputString) {
+  const letterBank = {};
+
+  for (let i = 0; i < inputString.length; i++) {
+    // support uppercase
+    const char = inputString[i].toLowerCase();
+
+    if (char !== ' ') {
+      if (letterBank[char]) { // if key exists for char
+        letterBank[char].push(i); // push i to existing index at key=char 
+  
+      } else {
+        letterBank[char] = [i]; // declare key with i if key does not exist yet
+      }
+    }
+  }
+  return letterBank;
+}
+
+console.log(letterPositions("lighthouse in the house"));
