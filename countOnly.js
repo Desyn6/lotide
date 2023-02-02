74fc8c207b1e5b4f5701d86e083ead591318042b
Denny <Dennyng1989@hotmail.com>
2023-02-01 16:03:23 -0800

    update countOnly test conditions

--- countOnly.js
+++ countOnly.js
@@ -15,33 +15,33 @@ const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
   /* second attempt revised by Ebenezer Igbinoba (mentor) */
-  // for (let item in itemsToCount) {
-  //   if (itemsToCount[item]) {
-  //     if(allItems.includes(item)) {
-  //       const itemCount = allItems.filter(el => el === item).length
-  //       results[item] = itemCount;
-  //     } else {
-  //       results[item] = 0;
-  //     }
-  //   }
-  // }
-  
-  /* attempt following exercise description */
-  /* loop through items in allItems */
-  for (const item of allItems) {
-    // check itemsToCount if items is truthy for counting
+  for (let item in itemsToCount) {
     if (itemsToCount[item]) {
-      // if item already exists in results, add 1
-      if (results[item]) {
-        results[item] += 1;
-      // if item does not exist in results, create item
+      if(allItems.includes(item)) {
+        const itemCount = allItems.filter(el => el === item).length
+        results[item] = itemCount;
       } else {
-        results[item] = 1;
+        results[item] = 0;
       }
     }
   }
+  
+  /* attempt following exercise description */
+  /* loop through items in allItems */
+  // for (const item of allItems) {
+  //   // check itemsToCount if items is truthy for counting
+  //   if (itemsToCount[item]) {
+  //     // if item already exists in results, add 1
+  //     if (results[item]) {
+  //       results[item] += 1;
+  //     // if item does not exist in results, create item
+  //     } else {
+  //       results[item] = 1;
+  //     }
+  //   }
+  // }
 
-  return results;
+  return results; 
 };
 
 const firstNames = [
@@ -59,7 +59,7 @@ const firstNames = [
 const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
 
 assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Karima"], 0);
 assertEqual(result1["Fang"], 2);
 assertEqual(result1["Agouhanna"], undefined);
 
