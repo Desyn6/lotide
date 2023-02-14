aaf515790d9ecea819727a4cdb573956a3ea8f02
Denny <Dennyng1989@hotmail.com>
2023-02-13 22:43:42 -0800

    commit recursive eqArrays to lotide repo

--- eqArrays.js
+++ eqArrays.js
@@ -21,17 +21,24 @@ const eqArrays = function(arrayOne, arrayTwo) {
 
   // loop to compare each element of array1/2
   for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) {
+    if (Array.isArray(arrayOne[i]) && Array.isArray(arrayTwo[i])) {
+      // recursively call eqArrays if array[i] is an array
+      if (!eqArrays(arrayOne[i], arrayTwo[i])) {
+        return false;
+      }
+    } else if (arrayOne[i] !== arrayTwo[i]) {
       return false;
     }
   }
-
-  // only true if none of the bools are false
+  // only true if no falses occur during iteration
   return true;
 };
-
 // test eqArrays using assertEqual
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should FAIL
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should FAIL
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => should FAIL
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should FAIL
+
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // => true
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])); // => false
+console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => false
